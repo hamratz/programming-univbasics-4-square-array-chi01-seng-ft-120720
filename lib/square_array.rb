0b7e0767b04f83c4d78f353ce38cb5fac3faecae
Marty Warfel <marthawarfel@gmail.com>
2020-11-25 06:04:12 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,7 +1,7 @@
 
-def square_array(array)
+def square_array(numbers)
   numbers = [1, 2, 3,]
-  new_numbers = [9, 10, 16, 25]
+  new_numbers = []
   
   numbers.length.times do |index|
   new_numbers.push(array[index] ** 2)
