4e7df3be64f3c442a6cbc031badff09b6636329f
Marty Warfel <marthawarfel@gmail.com>
2020-11-25 05:52:28 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,8 +1,8 @@
-numbers = [1, 2, 3]
-
-new_numbers = []
 
 def square_array(array)
+  numbers = [1, 2, 3]
+  new_numbers = []
+  
   numbers.length.times do |index|
   new_numbers.push(array[index] ** 2)
 end
